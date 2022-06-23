544a176290d377d0f98117deaf80d2ebd84fa1a4
Fuki Sato <fuki_sato@itsoft.com>
2022-06-23 10:33:01 +0900

    タスクエンティティを追加

---
+++ POMODORO-Timer/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POMODORO_Timer
+{
+    public class Tasks
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public int Priority { get; set; }
+        public bool IsCompleted { get; set; }
+        public Nullable<TimeSpan> Achievement { get; set; }
+    }
+}
