24752b850e8a2ac1cedbe549ce85957ea67a3388
Fuki Sato <fuki_sato@itsoft.com>
2022-06-23 10:47:52 +0900

    タスク用ファイルアクセス系クラス実装

---
+++ POMODORO-Timer/TaskFileAccessManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POMODORO_Timer
+{
+    public class TaskFileAccessManager : FileAccessManager
+    {
+        private string Path = System.IO.Path.Combine(Environment.CurrentDirectory, "Repository", "Task", "task.txt");
+        public string ErrorMsg = "";
+        public bool Load(out string contents)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path, Encoding.UTF8))
+                {
+                    contents = sr.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                contents = "";
+                ErrorMsg = ex.Message;
+                return false;
+            }
+            return true;
+        }
+
+        public bool Write(string contents)
+        {
+            try
+            {
+                File.WriteAllText(Path, contents, Encoding.UTF8);
+            }
+            catch (Exception ex)
+            {
+                ErrorMsg = ex.Message;
+                return false;
+            }
+            return true;
+        }
+    }
+}
